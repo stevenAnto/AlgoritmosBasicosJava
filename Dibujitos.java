322adfa5455e4569f8e3ed82a24f5ad64df1c3a6
STV <ecalcinap@unsa.edu.pe>
2022-03-19 17:13:42 -0500

    se hizo una cruz

--- Dibujitos.java
+++ Dibujitos.java
@@ -6,6 +6,7 @@ public class Dibujitos{
   public static void main(String []argumentos){
     //    first();
     second();
+    mostrarMatriz(figureMatriz(19,19));
 
   }
   public static void first(){
@@ -22,13 +23,40 @@ public class Dibujitos{
       for(int k=0;k<i;k++){
 	System.out.print(" ");
       }
-      System.out.println("**");
+      System.out.println(" ** ");
     }
     for(int l=6 ; l>0;l--){
       for(int m=l; m>0;m--){
 	System.out.print(" ");
       }
-	System.out.println("**");
+      System.out.println("**");
     }
   }
+  public static String [][] figureMatriz(int a, int b){
+    String  [][] matriz= new String [a][b];
+    int contador=0;
+    int numeroMedio= matriz.length/2;
+    for(int i =0;i<matriz.length;i++){
+      for(int j =0;j<matriz[i].length;j++){
+	if(i==numeroMedio||j==numeroMedio){
+	  matriz[i][j]="*";
+	}
+
+      }
+    }
+  return matriz;
+  }
+public static void mostrarMatriz(String [][]a){
+  for(int i =0;i<a.length;i++){
+
+    for(int j =0;j<a[i].length;j++){
+      if(a[i][j]!= null){
+	System.out.print(a[i][j] );
+
+      }
+      else {System.out.print(" ");}
+    }
+    System.out.print("\n");
+  }
+}
 }
